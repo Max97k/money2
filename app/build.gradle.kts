b185896faf3b770976144c235cdb1b224ab4aea4
Max97k <max97k@example.com>
2026-06-19 00:42:29 +0800

    fix: enable R8 minification and native debug symbols

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,14 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            optimization {
-                enable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
             }
         }
     }
